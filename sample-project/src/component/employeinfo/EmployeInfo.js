649b043b993010fbc890bbdcd8988a275097ace7
Kathir-S-10 <droptokathir@gmail.com>
2024-02-21 09:30:46 +0530

    empinfo

--- sample-project/src/component/employeinfo/EmployeInfo.js
+++ sample-project/src/component/employeinfo/EmployeInfo.js
@@ -1,7 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firebase/firebase.config';
+import { collection,getDocs } from 'firebase/firestore';
 
 
-//   return (
+const EmployeInfo = () => {
 
+  const [val,setVal]=useState([]);
+ 
+  const value=collection(db,"users")
+
+  useEffect(()=>{
+    const getData=async()=>{
+      const dbval=await getDocs(value)
+      setVal(dbval.docs.map(doc=>({...doc.data(),id:doc.id})))
+    }
+    getData()
+  })
+
+
+   
+
+  return (
     <div>
     <table>
       <thead>
@@ -26,5 +45,35 @@
     </div>
   );
 };
+
+export default EmployeInfo;
+
+
+//   return (
+
+//     <div>
+//     <table>
+//       <thead>
+//         <tr>
+//           <td>USERNAME</td>
+//           <td>EMAIL</td>
+//           <td>ROLE</td>
+//         </tr>
+//       </thead>
+//       <tbody>
+//           {val.map(details=>(
+//             <tr key={details.id}>
+//               <td>{details.userName}</td>
+//               <td>{details.email}</td>
+//               <td>{details.role}</td>
+//             </tr>
+//           ))}
+       
+//       </tbody>
+//     </table>
+     
+//     </div>
+//   );
+// };
 
 
